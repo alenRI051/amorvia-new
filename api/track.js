41cd668bdef21f1eac2dff6533b2e4c6095e2b3e
alenRI051 <alen051@yahoo.com>
2025-08-22 20:44:31 +0200

    Delete api/track.js

--- api/track.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  try {
-    res.setHeader('Cache-Control', 'no-store');
-    res.status(204).end();
-  } catch (e) {
-    res.status(200).json({ ok: true });
-  }
-};
