a1ca421e65cb83898060f7436c92395b8765a160
alenRI051 <alen051@yahoo.com>
2025-09-02 13:08:20 +0200

    Update track.ts

--- api/track.ts
+++ api/track.ts
@@ -1,4 +1,1 @@
-export const config = { runtime: 'edge' };
-export default async function handler(_req: Request): Promise<Response> {
-  return new Response(null, { status: 204, headers: { 'Cache-Control': 'no-store' } });
-}
+
