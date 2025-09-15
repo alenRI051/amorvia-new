533ac587f5660a56025ebe6b61f8cbe4cfa57678
alenRI051 <alen051@yahoo.com>
2025-09-15 21:58:29 +0200

    Update sw-register.js

--- public/js/sw-register.js
+++ public/js/sw-register.js
@@ -1,7 +1,22 @@
-(function () {
-  const url = new URL(location.href);
-  const skipSW = url.searchParams.has('nosw') || navigator.webdriver;
-  if ('serviceWorker' in navigator && !skipSW) {
-    navigator.serviceWorker.register('/sw.js');
+// /public/js/sw-register.js
+(() => {
+  const isProdHost = location.hostname === 'amorvia.eu';
+  const skip = location.search.includes('nosw=1') ||
+               !isProdHost ||                    // only run on prod host
+               !('serviceWorker' in navigator);
+
+  if (skip) {
+    console.log('[SW] skip register (dev/nosw/non-prod)');
+    // If a SW was previously installed, unregister it in dev:
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations?.().then(regs => regs.forEach(r => r.unregister()));
+    }
+    return;
   }
+
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(err => {
+      console.warn('[SW] register failed', err);
+    });
+  });
 })();
