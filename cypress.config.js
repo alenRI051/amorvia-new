9aa991c42a3312f48e5e7ac6ebe9344b67bcd22b
alenRI051 <alen051@yahoo.com>
2025-10-05 21:53:40 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,45 +1,17 @@
-const { defineConfig } = require('cypress');
-const path = require('path');
-const http = require('http');
-const finalhandler = require('finalhandler');
-const serveStatic = require('serve-static');
-
-function startStaticServer({ port = 3000, root = path.join(__dirname) } = {}) {
-  const serve = serveStatic(root);
-  const server = http.createServer((req, res) => serve(req, res, finalhandler(req, res)));
-  return new Promise((resolve) => {
-    server.listen(port, () => resolve({
-      close: () => new Promise((r) => server.close(() => r()))
-    }));
-  });
-}
-
-let serverRef;
-
-module.exports = defineConfig({
+// cypress.config.js (CommonJS, no require('cypress'))
+module.exports = {
   e2e: {
-    specPattern: 'cypress/e2e/**/*.cy.js',
     baseUrl: 'http://localhost:3000',
-    chromeWebSecurity: false,
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    viewportWidth: 1280,
+    viewportHeight: 720,
     video: false,
-    setupNodeEvents(on, config) {
-      on('task', {
-        async startServer() {
-          if (!serverRef) {
-            serverRef = await startStaticServer({ root: __dirname, port: 3000 });
-            return true;
-          }
-          return true;
-        },
-        async stopServer() {
-          if (serverRef) {
-            await serverRef.close();
-            serverRef = null;
-          }
-          return true;
-        }
-      });
-      return config;
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 20000,
+    env: {
+      // let tests point to your app URL if needed
+      APP_URL: 'http://localhost:3000',
     },
   },
-});
+};
