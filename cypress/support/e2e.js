18eed35a4edcaf75e730a4926ff6d560df01bd55
alenRI051 <alen051@yahoo.com>
2025-10-05 20:01:30 +0200

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,10 @@
 // cypress/support/e2e.js
+// Load custom commands and set global hooks if needed.
 import './commands';
+
+// (Optional) log console errors to help debug CI flakes
+Cypress.on('uncaught:exception', (err) => {
+  // Returning false here prevents Cypress from failing the test on app errors.
+  // Flip to `true` if you want app exceptions to fail tests.
+  return false;
+});
