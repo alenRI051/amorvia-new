90e59e9ae8429b951afacb1323306448b853a80f
alenRI051 <alen051@yahoo.com>
2025-10-05 21:00:02 +0200

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,10 +1,10 @@
-// cypress/support/e2e.js
-// Load custom commands and set global hooks if needed.
 import './commands';
 
-// (Optional) log console errors to help debug CI flakes
-Cypress.on('uncaught:exception', (err) => {
-  // Returning false here prevents Cypress from failing the test on app errors.
-  // Flip to `true` if you want app exceptions to fail tests.
-  return false;
+// Boot a fresh app for every test
+beforeEach(() => {
+  cy.task('startServer');
+  cy.clearCookies();
+  cy.clearLocalStorage();
+  // disable SW + cache like you do manually
+  cy.visit('/index.html?nosw=1&devcache=0');
 });
