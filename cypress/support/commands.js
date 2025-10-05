f63b0c9653f43fdc16aea4c6cdd70536563df3ca
alenRI051 <alen051@yahoo.com>
2025-10-05 19:25:15 +0200

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -14,3 +14,14 @@ Cypress.Commands.add('seeAct', (n) => {
 Cypress.Commands.add('selectScenario', (title) => {
   cy.get('#scenarioPicker').select(title, { force: true });
 });
+
+// cypress/support/commands.js
+Cypress.Commands.add('bootScenario', (scenarioId, url = '/?nosw=1&devcache=0') => {
+  cy.visit(url, {
+    onBeforeLoad(win) {
+      win.localStorage.setItem('amorvia:lastScenario', scenarioId);
+    },
+  });
+  cy.get('#dialog', { timeout: 20000 }).should('be.visible');
+  cy.get('#choices', { timeout: 20000 }).children().should('have.length.greaterThan', 0);
+});
