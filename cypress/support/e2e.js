ead44e6cb5e71032c058e6c0ad5f70d22a00014c
alenRI051 <alen051@yahoo.com>
2025-10-05 23:21:45 +0200

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,7 +1,6 @@
 // cypress/support/e2e.js
 import './commands';
 
-// Start the static server once for this spec; stop when done.
 before(() => {
   cy.task('startServer');
 });
@@ -10,8 +9,8 @@ after(() => {
   cy.task('stopServer');
 });
 
-// For each test: clean state and (re)open the app’s root.
 beforeEach(() => {
   cy.clearLocalStorage();
   cy.visit('/');
 });
+
