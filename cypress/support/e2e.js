7c9ad8a5e1170623ffff2a2846747b8d0a0a91c0
alenRI051 <alen051@yahoo.com>
2025-10-06 16:14:48 +0200

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,16 +1,14 @@
 // cypress/support/e2e.js
+
+// Cypress 10+ support file
 import './commands';
 
-before(() => {
-  cy.task('startServer');
-});
-
-after(() => {
-  cy.task('stopServer');
-});
+// If you have cy.task('startServer') wired in cypress.config.js, you can keep it.
+// If not, rely on http-server started externally or skip this hook entirely.
 
 beforeEach(() => {
+  // Ensure a clean slate and v2 mode each test
   cy.clearLocalStorage();
-  cy.visit('/');
+  // Boot the specific scenario used by your spec(s)
+  cy.bootScenario('dating-after-breakup-with-child-involved');
 });
-
