33932fb1f8e2078ed2cdd9568c2dcfd55429f8d2
alenRI051 <alen051@yahoo.com>
2025-11-14 19:04:02 +0100

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,11 +1,7 @@
 // cypress/support/e2e.js
-// Minimal setup for Amorvia + Cypress v15
-
 import './commands';
 
 Cypress.on('uncaught:exception', (err) => {
-  // Ako želiš, možemo ignorirati neke benigne error-e iz third-party skripti
-  // Za sada ih ne utišavamo, samo log:
-  // console.error('Uncaught exception:', err);
-  return false; // ili true ako želiš da test PUKNE na error
+  // Za sada ih ignoriramo u testovima mini-enginea
+  return false;
 });
